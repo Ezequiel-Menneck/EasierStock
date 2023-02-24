edee7f2672759fd217ad3aac86c3dd6327d8beef
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-02-24 12:04:37 -0300

    project setup

---
+++ src/main/java/com/easierstock/easierstock/EasierStockApplication.java
@@ -0,0 +1,13 @@
+package com.easierstock.easierstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasierStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasierStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easierstock/easierstock/EasierStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easierstock.easierstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasierStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
